9895ef2ae08f6f9cda9a0c650a21e7b095311874
Cleankz <92687535+Cleankz@users.noreply.github.com>
2022-04-01 20:22:47 +0300

    Create test_dynarray.py

---
+++ test_dynarray.py
@@ -0,0 +1,28 @@
+import unittest
+import random
+from dynarray import DynArray
+
+class MyTests(unittest.TestCase):
+    
+    def test_insert(self):
+        da = DynArray()
+        for i in range(5):
+            da.append(i)
+        for j in range(10):
+            da.insert(random.randint(0,4),random.randint(1,14))
+        self.assertEqual(16,da.capacity)
+        for j in range(20):
+            da.insert(random.randint(0,15),random.randint(0,15))
+        self.assertNotEqual(16,da.capacity)
+
+    def test_delete(self):
+        da = DynArray()
+        for i in range(random.randint(0,500)):
+            da.append(i)
+        for j in range(random.randint(0,500)):
+            da.delete(i)
+
+    
+
+if __name__ == '__main__':
+    unittest.main()
